2e4ea85b200142cf88912516909c2a508e17310b
Jafet Alvarado <56947979+yt992j@users.noreply.github.com>
2020-12-05 16:04:58 -0600

    correción logo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBEe3bOyo4CeZkMNVvhh_BbnqukrHolkPU",
+    authDomain: "frontend-9ced7.firebaseapp.com",
+    databaseURL: "https://frontend-9ced7.firebaseio.com",
+    projectId: "frontend-9ced7",
+    storageBucket: "frontend-9ced7.appspot.com",
+    messagingSenderId: "1079145043209",
+    appId: "1:1079145043209:web:f9f7a5f88fa53137a1efd8"
+  }
 };
